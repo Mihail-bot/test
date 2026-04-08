99a91bcf5e8b941f956469fd3f84c79695fde4f3
Miha <shmihail85@yandex.ru>
2026-04-08 13:08:39 +0300

    initial git

---
+++ src/main/java/sky/pro/hogvards/book/HogvardsBookApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.hogvards.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogvardsBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogvardsBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/hogvards/book/HogvardsBookApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.hogvards.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogvardsBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
